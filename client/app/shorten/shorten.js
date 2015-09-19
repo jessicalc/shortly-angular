5b940782d969e99ec6621eaaeaf7f5e8043e5915
jessicalc <jessicalaurenchong@gmail.com>
2015-09-18 20:48:33 -0700

    Links are now unique!

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -14,4 +14,23 @@ angular.module('shortly.shorten', [])
       $scope.loading = false;
     });
   };
-});
+})
+
+.directive('ensureUnique', ['$http', function($http) {
+  return {
+    require: 'ngModel',
+    link: function(scope, ele, attrs, c) {
+      scope.$watch(attrs.ngModel, function() {
+        $http({
+          method: 'POST',
+          url: '/api/links/' + attrs.ensureUnique,
+          data: {'url': attrs.ensureUnique} 
+        }).success(function(data,status, headers, cfg) {
+          c.$setValidity('unique', data.isUnique);
+        }).error(function(data, status, headers, cfg) {
+          c.$setValidity('unique', false);
+        });
+      });
+    }
+  }
+}]);
